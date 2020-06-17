94c7729545a1a9509929fe91d5fe7dfebda96c4f
zhanxc <1974346076@qq.com>
2020-06-17 21:41:35 +0800

    修改了部分代码

--- test.py
+++ test.py
@@ -4,15 +4,27 @@ def uidPrint(useridlist):
     for uid in useridlist:
         #打印uid信息
         print("useridlist",uid)
-#定义一个用户id列表
+        #
+    #
+
+#定义一个用户id有序列表
 userid=[1,2,3,4,5]
-#定义一个用户名列表
-username=("1","2","3")
 #赋值以前
 uidPrint(userid)
-
 print("----------")
-
 #赋值以后
 userid[1]=100
 uidPrint(userid)
+userid.pop()
+userid.append(99)
+userid[2] = 22
+#定义一个用户名不可变有序列表
+username=("1","2","3")
+uidPrint(username)
+#username初始化后不可修改
+#定义一个dict对象
+dog={"name":"big yellow dog","sex":"female","age":19}
+#定义一个set对象，提供list输入
+passwd=([1,2,3,4])
+passwd.append(21)
+passwd.remove(1)
