25d25d21f672bddd6dd151f4556bda374b6746d8
zhanxc <1974346076@qq.com>
2020-06-17 18:45:57 +0800

    modify test.py

--- test.py
+++ test.py
@@ -2,11 +2,17 @@
 #function print
 def uidPrint(useridlist):
     for uid in useridlist:
-    print("useridlist",uid)
-#
+        #打印uid信息
+        print("useridlist",uid)
+#定义一个用户id列表
 userid=[1,2,3,4,5]
-#
+#定义一个用户名列表
 username=("1","2","3")
+#赋值以前
 uidPrint(userid)
+
+print("----------")
+
+#赋值以后
 userid[1]=100
 uidPrint(userid)
