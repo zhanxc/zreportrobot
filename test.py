88735e30265c4e7f701bee6189aec9b76dd53f81
zhanxc <1974346076@qq.com>
2020-06-28 18:04:44 +0800

    增加了部分函数

--- test.py
+++ test.py
@@ -1,3 +1,70 @@
+#测试
+#FLASK测试
+""" from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return '<h1>Home</h1>'
+
+@app.route('/signin', methods=['GET'])
+def signin_form():
+    return '''<form action="/signin" method="post">
+              <p><input name="username"></p>
+              <p><input name="password" type="password"></p>
+              <p><button type="submit">Sign In</button></p>
+              </form>'''
+
+@app.route('/signin', methods=['POST'])
+def signin():
+    # 需要从request对象读取表单内容：
+    if request.form['username']=='admin' and request.form['password']=='password':
+        return '<h3>Hello, admin!</h3>'
+    return '<h3>Bad username or password.</h3>'
+
+if __name__ == '__main__':
+    app.run()
+
+@app.route('/welcome', methods=['GET', 'POST'])
+def welcome():
+    return '<h1>Welcome Home</h1>' """
+#urllib测试
+#不添加请求头部
+""" from urllib import request
+
+with request.urlopen('https://book.douban.com/subject/34986715/?icn=index-latestbook-subject') as f:
+    data = f.read()
+    print('Status:', f.status, f.reason)
+    for k, v in f.getheaders():
+        print('%s: %s' % (k, v))
+    print('Data:', data.decode('utf-8')) """
+#添加了请求头部
+"""from urllib.request import urlopen, Request
+
+url = 'https://book.douban.com/subject/34986715/?icn=index-latestbook-subject'
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'}
+ret = Request(url, headers=headers)
+res = urlopen(ret)
+aa = res.read().decode('utf-8')
+print(aa)"""
+#
+#读取JSON数据，然后解析为python对象
+""" from urllib import request
+from urllib.parse import urlparse
+
+url='https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20%3D%202151330&format=json'
+def fetchData(url):
+    data = request.urlopen(url)
+    #返回JSON数据
+    print(data)
+    parseData = urlparse(url)
+    #解析URL数据，变成一个python对象（tuble）
+    print(parseData)
+
+#代码测试
+fetchData(url) """
 #list
 #function print
 def uidPrint(useridlist):
@@ -6,7 +73,8 @@ def uidPrint(useridlist):
         print("useridlist",uid)
         #
     #
-
+#python数据类型测试
+#
 #定义一个用户id有序列表
 userid=[1,2,3,4,5]
 #赋值以前
@@ -24,7 +92,10 @@ uidPrint(username)
 #username初始化后不可修改
 #定义一个dict对象
 dog={"name":"big yellow dog","sex":"female","age":19}
+print(dog)
 #定义一个set对象，提供list输入
 passwd=([1,2,3,4])
+print(passwd)
 passwd.append(21)
 passwd.remove(1)
+#
--- zabbixReport.py
+++ zabbixReport.py
@@ -1,1 +1,94 @@
-#
+#测试zabbix api接口
+
+# from urllib.request import urlopen, Request
+import urllib
+# import urllib.parse
+import json
+import requests
+import csv
+import datetime
+import time
+import codecs
+#ZABBIX API
+url = 'http://zabbix_servverip/api_jsonrpc.php'
+headers = {"Content-Type": "application/json-rpc"}
+user = '管理员'
+passwd = '管理员密码'
+# 定义报告表头
+csvheader = []
+# 时间戳
+x = (datetime.datetime.now()-datetime.timedelta(minutes=120)).strftime("Y%-%m-%d %H:%M:%S")
+y = (datetime.datetime.now()).strftime("Y%-%m-%d %H:%M:%S")
+def gettoken():
+    data = {"jsonrpc":"2.0","method":"user.login","params":{"user":user,"password":passwd},"id":1,"auth":None}
+    auth = requests.post(url=url,headers=headers,json=data)
+    return json.loads(auth.content)['result']
+
+def timestamps(x,y):
+    p = time.strptime(x,"Y%-%m-%d %H:%M:%S")
+    starttime = str(int(time.mktime(p)))
+    q = time.strptime(y,"Y%-%m-%d %H:%M:%S")
+    endtime = str(int(time.mktime(q)))
+    return starttime,endtime
+
+def login():
+    # url='http://zabbix_servverip/api_jsonrpc.php'
+    # headers={"Content-Type": "application/json-rpc"}
+    # 字典转json
+    data = '{"jsonrpc":"2.0","method":"user.login","params":{"user":user,"password":passwd},"id":1,"auth":null}'
+    # print(data)
+    #请求参数
+    data = json.loads(data)
+    print(data)
+    data = bytes(urllib.parse.urlencode(data),encoding='utf-8')
+    ret = urllib.request.Request(url, headers=headers,data=data)
+    res = urllib.request.urlopen(ret)
+    aa = res.read().decode('utf-8')
+    print(aa)
+
+def logout(auth):
+    data = {"jsonrpc":"2.0","method":"user.logout","params":[],"id":1,"auth":auth}
+    auth = requests.post(url=url,headers=headers,json=data)
+    return json.loads(auth.content)
+
+def gethosts(groupids,auth):
+    data = {
+        "jsonrpc":"2.0",
+        "method":"host.get",
+        "params":{
+            "output": ["name"],
+            "groupids": groupids,
+            "filter": {
+                "status": "0"
+            },
+            "selectInterface": [
+                "ip"
+            ]
+        },
+        "auth": auth,
+        "id": 1
+    }
+    gethost = requests.post(url=url,headers=headers,json=data)
+    return json.loads(gethost.content)['result']
+
+def gethist(gethosts,token,timestamp):
+    pass
+
+def writecsv(gethist):
+    pass
+
+def getzabbixapiversion(url,headers,data):
+    ret = urllib.request.Request(url, headers=headers,data=data)
+    res = urllib.request.urlopen(ret)
+    aa = res.read().decode('utf-8')
+    print(aa)
+    #
+# getzabbixapiversion()
+# login()
+# 函数调用逻辑
+# token = gettoken()
+# timestamp = timestamps(x,y)
+# gethosts = gethosts(groupids,token)
+# gethist = gethist(gethosts,token,timestamp)
+# writecsv(gethist)
+# logout(token)
